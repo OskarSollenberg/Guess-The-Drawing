6f173f60060f6789661dfda7ee9af68d476f3edc
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2023-12-04 17:28:21 +0100

    Made some functions for buttons in the cards and some quick styling on css

--- script.js
+++ script.js
@@ -4,30 +4,47 @@
 // const startButton = document.querySelector(".start-game"); //define the start button
 
 async function fetchRandomAnimal() {
-  const url = "https://animal-name-api.onrender.com/random-animal";
-  const options = {
-    method: "GET",
-  };
+    const url = "https://animal-name-api.onrender.com/random-animal";
+    const options = {
+        method: "GET",
+    };
 
-  try {
-    const response = await fetch(url, options);
-    const result = await response.json();
-    return result?.animal;
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        return result?.animal;
+    } catch (error) {
+        console.error(error);
+    }
 }
 // Function to start the drawing phase
 async function startDrawing() {
-  let currentWord = await fetchRandomAnimal();
-  //   wordDisplay.textContent = "Draw: " + currentWord;
-  //   wordDisplay.style.display = "block"; // Show the word
-  //   startButton.style.display = "none"; // Hide the start button
+    let currentWord = await fetchRandomAnimal();
+    //   wordDisplay.textContent = "Draw: " + currentWord;
+    //   wordDisplay.style.display = "block"; // Show the word
+    //   startButton.style.display = "none"; // Hide the start button
 
-  //   setTimeout(() => {
-  //     // Hide the word after 2 seconds
-  //     wordDisplay.style.display = "none";
-  //     canDraw = true;
-  //     startTimer(); // Start the countdown timer
-  //   }, 2000);
+    //   setTimeout(() => {
+    //     // Hide the word after 2 seconds
+    //     wordDisplay.style.display = "none";
+    //     canDraw = true;
+    //     startTimer(); // Start the countdown timer
+    //   }, 2000);
+}
+
+// const page1 = document.querySelector("#page1");
+// const page2 = document.querySelector("#page2");
+// const page3 = document.querySelector("#page3");
+
+const pages = document.querySelectorAll(".card");
+const pageButtons = document.querySelectorAll(".button");
+let currentPageNumber = 0;
+
+for (let button of pageButtons) {
+    button.addEventListener("click", toggleVisability);
+}
+function toggleVisability() {
+    pages[currentPageNumber].classList.remove("card-visable");
+    pages[currentPageNumber + 1].classList.add("card-visable");
+    currentPageNumber++;
 }
