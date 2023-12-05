9b2398334dd98e97b118de84f920b41f587f3bc3
Bwunkaz <122973984+Bwunkaz@users.noreply.github.com>
2023-12-05 12:00:13 +0100

    Page 1 to 4 with syling and functionality

--- script.js
+++ script.js
@@ -44,3 +44,79 @@ function toggleVisability() {
     pages[currentPageNumber + 1].classList.add("card--visable");
     currentPageNumber++;
 }
+
+const canvasEl = document.querySelector("#canvas");
+const context = canvasEl.getContext("2d");
+let isDrawing = false;
+let canDraw = true;
+let lastX = 0;
+let lastY = 0;
+let seconds = 21;
+let rect = canvasEl.getBoundingClientRect();
+
+// canvasEl.width = rect.width * devicePixelRatio;
+// canvasEl.height = rect.height * devicePixelRatio;
+context.scale(devicePixelRatio, devicePixelRatio);
+context.lineWidth = 2; // Set the line width to make it thicker
+context.strokeStyle = "#000"; // Set the line color to black
+context.lineJoin = "round"; // Round the corners when two lines meet
+context.lineCap = "round"; // Round the ends of the lines
+let heightRatio = 0.7;
+canvasEl.height = canvasEl.width * heightRatio;
+
+/* timer
+    setInterval method calls a function at specified intervals.
+    It takes as parameters:
+    1) a function to be executed every delay milliseconds and
+    2) milliseconds
+    it returns an interval ID which identifies the interval
+    and can be later removed by calling clearInterval() that takes
+    as a parameter a function that serves as first parameter in
+    setInterval() method.
+*/
+setInterval(function () {
+    if (seconds <= 0) {
+        canDraw = false;
+    } else {
+        seconds -= 1;
+        document.querySelector("#counter").textContent = seconds;
+    }
+}, 1000);
+canvasEl.addEventListener("pointerdown", function (e) {
+    isDrawing = true;
+    // const x = e.clientX - canvasEl.offsetLeft;
+    // const y = e.clientY - canvasEl.offsetTop;
+    //   const x = ((e.offsetX * canvasEl.width) / canvasEl.clientWidth) | 0;
+    //   const y = ((e.offsetY * canvasEl.height) / canvasEl.clientHeight) | 0;
+    lastX = ((e.offsetX * canvasEl.width) / canvasEl.clientWidth) | 0;
+    lastY = ((e.offsetY * canvasEl.height) / canvasEl.clientHeight) | 0;
+    context.beginPath();
+    //   context.moveTo(x, y);
+    context.moveTo(lastX, lastY);
+});
+canvasEl.addEventListener("pointermove", function (e) {
+    if (canDraw && isDrawing) {
+        // const x = e.clientX - canvasEl.offsetLeft;
+        // const y = e.clientY - canvasEl.offsetTop;
+        const x = ((e.offsetX * canvasEl.width) / canvasEl.clientWidth) | 0;
+        const y = ((e.offsetY * canvasEl.height) / canvasEl.clientHeight) | 0;
+        context.quadraticCurveTo(
+            lastX,
+            lastY,
+            (lastX + x) / 2,
+            (lastY + y) / 2
+        );
+        lastX = x;
+        lastY = y;
+        // context.lineTo(x, y);
+        context.stroke();
+    }
+});
+canvasEl.addEventListener("pointerup", function () {
+    if (isDrawing) {
+        context.lineTo(lastX, lastY);
+        context.stroke();
+        context.closePath();
+        isDrawing = false;
+    }
+});
